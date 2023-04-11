37eac7920c2bca58257e55187dbec0e385a5d49a
19DennisBrown <107542527+19DennisBrown@users.noreply.github.com>
2023-04-11 08:10:01 +0300

    New files and their content

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './Styles/App.css';
 import Header from './Header.js';
 import Main from './Main.js';
+import Aside from './Aside.js';
 import Footer from './Footer.js';
+import {Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      <Main/>
+      <Routes>
+        <Route path='/' element={<Main/>}/>
+        <Route path='/aside' element={<Aside/>}/>
+      </Routes>
       <Footer/>
     </div>
   );
--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,15 @@
 
 import React from 'react'
 import './Styles/Header.css'
+import {NavLink} from 'react-router-dom'
+
 
 const Header = () => {
   return (
     <div className='container-header'>
       Header
+      <NavLink to='Main.js'>Main</NavLink>
+      <NavLink to='Aside.js'>Aside</NavLink>
     </div>
   )
 }
