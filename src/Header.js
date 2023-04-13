42d1691d245e55fb688032cda517dce14568d328
19DennisBrown <107542527+19DennisBrown@users.noreply.github.com>
2023-04-13 09:13:46 +0300

    Additional_files

--- src/Header.js
+++ src/Header.js
@@ -1,14 +1,20 @@
 
 import React from 'react'
 import './Styles/Header.css'
-import {NavLink} from 'react-router-dom'
 import Navs from './Navs.js'
+import Logo from './gymimages/community.jpg'
 
 
 const Header = () => {
+
   return (
     <div className='container-header'>
-      Header
+      <section className="right-header">
+        <img src={Logo} alt="logo image" className='logo' />
+        <h1 className="logo-title">
+          Our Gym
+        </h1>
+      </section>
       <Navs/>
     </div>
   )
